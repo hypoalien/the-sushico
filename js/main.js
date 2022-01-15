c80306fb05a5f8a1af1b055986bd9a7380357d09
hypoalien <61846455+hypoalien@users.noreply.github.com>
2022-01-15 19:40:09 +0530

    Update main.js
    Date updated

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
     var ssFinalCountdown = function() {
 
         var finalDate =  new Date("December 4, 2021 15:37:25").getTime();
-        //-date: "Mar 25 2021",
+        //-date: "Jan 24 2022",
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
